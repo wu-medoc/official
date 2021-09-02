1aaba59b1335573621f24610d71a5273d60764fb
mei.wu <mei.wu@eyesmedia.com.tw>
2021-09-02 16:50:01 +0800

    set cookie

--- assets/js/main-mobiifin.js
+++ assets/js/main-mobiifin.js
@@ -100,49 +100,64 @@ var openUrl = function(url) {
 }
 // get cookie
 var checkCookie = function () {
-	let userCode = getCookie("userCode");
+	let userCode = "";
+	// app get param
+	const urlParams = new URLSearchParams(location.search);
+	const userCodeApp = urlParams.get("userCode");
+	if(userCodeApp != "") {
+		userCode = userCodeApp;
+		setCookie("userCode", userCode, 30);
+	}else {
+		userCode = getCookie("userCode");
+	}
 	const userData = {
 		"UserInfo_Code": userCode
 	}
+
 	console.log(userCode);
 	if (userCode != "" && userCode != null) {
-		let temp = async function() {
-			let env = ""
-			switch (location.hostname) {
-				case "sit.mobii.ai":
-					env = "https://sit-afpapi.mobii.ai/api/Event/EventUserLog"
-					break;
-				case "www-uat.mobii.ai":
-					env = "https://afpapi-uat.mobii.ai/api/Event/EventUserLog"
-					break;
-				case "www.mobii.ai":
-					env = " https://eventsapi.mobii.ai/Event/EventUserLog"
-					break;
-				case "mobii.ai":
-					env = " https://eventsapi.mobii.ai/Event/EventUserLog"
-					break;
-				default:
-					env = "https://sit-afpapi.mobii.ai/api/Event/EventUserLog"
-					break;
-			}
-			// console.log(env);
-			await fetch(env, {
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify(userData),
-				method: 'POST'
-			}).then(response => response.json())
-			.then(data => {
-				// 	// console.log(data);
+		let env = ""
+		switch (location.hostname) {
+			case "127.0.0.1":
+				env = "http://54.150.124.230:38089/Event/EventUserLog"
+				break;
+			case "sit.mobii.ai":
+				env = "http://54.150.124.230:38089/Event/EventUserLog"
+				break;
+			case "www.mobii.ai":
+				env = "https://eventsapi.mobii.ai/Event/EventUserLog"
+				break;
+			case "mobii.ai":
+				env = "https://eventsapi.mobii.ai/Event/EventUserLog"
+				break;
+			default:
+				env = ""
+				break;
+		}
+		// console.log(env);
+		if(env != "") {
+			let temp = async function() {
+				await fetch(env, {
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify(userData),
+					method: 'POST'
+				}).then(response => response.json())
+				.then(data => {
+					// console.log(data);
 					var myFin = new bootstrap.Modal(document.getElementById('myFin'));
 					myFin.show();
 					setTimeout(function(){ 
 						location.href = 'https://www.sinotrade.com.tw/openact?strProd=0113&strWeb=0214&utm_campaign=OP_TSP_01&utm_source=Mobii&utm_medium=button_0816';
 					}, 500);
-			});
+				});
+			}
+			temp();
+		} else {
+			console.log(location.hostname + "無法Log");
+			location.href = 'https://www.sinotrade.com.tw/openact?strProd=0113&strWeb=0214&utm_campaign=OP_TSP_01&utm_source=Mobii&utm_medium=button_0816';
 		}
-		temp();
 	} else {
 		var myModal = new bootstrap.Modal(document.getElementById('myModal'));
 		myModal.show();
@@ -163,4 +178,11 @@ var getCookie = function(userCode) {
 		}
 	}
 	return "";
+}
+
+var setCookie = function(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
